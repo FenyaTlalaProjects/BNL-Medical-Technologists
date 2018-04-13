71ce462f931bfc8f9f6916c1f72a5b517e28ecef
Mohapi Mokoena <mohapi.mokoena@integrove.com>
2018-04-13 22:28:52 +0200

    First commit

---
+++ src/main/java/za/co/bnl/medicaltechnologies/MedicalTechnologiesApplication.java
@@ -0,0 +1,12 @@
+package za.co.bnl.medicaltechnologies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalTechnologiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalTechnologiesApplication.class, args);
+	}
+}
